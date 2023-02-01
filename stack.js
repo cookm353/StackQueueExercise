6703fd643c90bff042f2b523c1cd320d81bec02e
Matthew Cook <cookm353@gmail.com>
2023-02-01 18:06:51 -0500

    Donezo

--- stack.js
+++ stack.js
@@ -20,26 +20,51 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newFirst = new Node(val)
+    
+    if (this.size === 0) {
+      this.last = newFirst
+    }
+    
+    newFirst.next = this.first
+    this.first = newFirst
+    
+    this.size ++
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.size === 0) {
+      throw new Error
+    }
 
+    let first = this.first
+
+    if (this.size === 1) {
+      this.first = null
+      this.last = null
+    }
+    
+    this.size--
+    return first.val
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
+    if (this.size === 0) {
+      throw new Error
+    }
 
+    return this.first.val
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0
   }
 }
 
