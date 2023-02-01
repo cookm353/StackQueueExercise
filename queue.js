27711c255150991f21060ed23be3cca28826c37d
Matthew Cook <cookm353@gmail.com>
2023-02-01 17:47:35 -0500

    Donezo

--- queue.js
+++ queue.js
@@ -20,27 +20,62 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newEnd = new Node(val)
+    
+    // If the queue is empty...
+    if (this.size === 0) {
+      this.first = newEnd
+      this.last = newEnd
+    } 
 
+    this.last.next = newEnd
+    this.last = newEnd
+    this.size++
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) {
+      throw new Error
+    } 
+    
+    let firstNode = this.first
 
+    if (this.size === 1) {
+      this.first = null
+      this.last = null
+    } else {
+      this.first = this.first.next
+    }
+
+    this.size --
+
+    return firstNode.val
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    if (this.size === 0) {
+      throw new Error
+    }
+    return this.first.val
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0
   }
 }
 
 module.exports = Queue;
+
+const queue = new Queue()
+queue.enqueue(10)
+queue.enqueue(100)
+queue.enqueue(1000)
+console.log(queue.first.val)
+console.log(queue.first.next.val)
